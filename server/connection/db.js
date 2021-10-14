732974cfd9db2dda313bb92bdd3850ff91aaca4d
Ansh Desai <anshdesai20@gmail.com>
2021-10-14 13:16:56 -0400

    Update db.js

--- server/connection/db.js
+++ server/connection/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const Connection = async (username, password) => {
     try {   
-        const URL = `mongodb://ansh:randomforest@inshorts-shard-00-00.3iyba.mongodb.net:27017,inshorts-shard-00-01.3iyba.mongodb.net:27017,inshorts-shard-00-02.3iyba.mongodb.net:27017/inshorts?ssl=true&replicaSet=atlas-sjaf2y-shard-0&authSource=admin&retryWrites=true&w=majority`;
+        const URL = `mongodb://`;
         
         await mongoose.connect(URL, { useNewUrlParser: true })
         
